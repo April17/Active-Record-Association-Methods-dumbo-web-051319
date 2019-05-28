120a4bf05e15992fa894d8215840268f41adddf0
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-28 01:26:01 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,10 +4,10 @@ class Song < ActiveRecord::Base
 
   def get_genre_name
     Genre.find_by(id: self.genre_id).name
-    # binding.pry
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    binding.pry
   end
 end
